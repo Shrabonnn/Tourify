a704c5fed1540f07232282d85c09046482d279d0
Shrabonnn <138008141+Shrabonnn@users.noreply.github.com>
2024-09-10 12:40:54 +0600

    navbar
    navbar

--- admin_Data(2).php
+++ admin_Data(2).php
@@ -101,29 +101,30 @@
           
         </ul>
       </li><!-- End Components Nav -->
-
+      
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="admin_Data(1).php">
-              <i class="bi bi-circle"></i><span>Users Table</span>
-            </a>
-          </li>
-          <li>
-            <a href="admin_Data.php">
-              <i class="bi bi-circle.php"></i><span>Booked Table</span>
-            </a>
-          </li>
-          <li>
-            <a href="admin_Data(2).php">
-              <i class="bi bi-circle"></i><span>Packages Table</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
+  <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+    <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+  </a>
+  <ul id="tables-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+    <li>
+      <a href="admin_Data(1).php">
+        <i class="bi bi-circle"></i><span>Users Table</span>
+      </a>
+    </li>
+    <li>
+      <a href="admin_Data.php">
+        <i class="bi bi-circle"></i><span>Booked Table</span>
+      </a>
+    </li>
+    <li>
+      <a href="admin_Data(2).php">
+        <i class="bi bi-circle"></i><span>Packages Table</span>
+      </a>
+    </li>
+  </ul>
+</li>
+<!-- End Tables Nav -->
 
 
       <li class="nav-heading">Pages</li>
